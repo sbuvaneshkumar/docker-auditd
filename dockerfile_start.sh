ee11335085e8520f8ae39bef84d1fc9581e83928
Nicolas Dordet <dordet.n@gmail.com>
2016-07-13 13:29:53 +0200

    clean up

--- dockerfile_start.sh
+++ dockerfile_start.sh
@@ -4,9 +4,5 @@
 
 docker build \
 --no-cache \
---build-arg SUBSCRIPTION_USER=user \
---build-arg SUBSCRIPTION_PASSWORD=password \
---build-arg SUBSCRIPTION_POOL=poolid \
---build-arg HOSTNAME=master1.ose-exemple.com \
--t docker.io/ndox/auditd:v1 \
+-t ndox/auditd:v1 \
 .
