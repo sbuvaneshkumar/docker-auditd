873c1999d8f56254a5e32ad0f3a159082bc4ada1
Nicolas Dordet <dordet.n@gmail.com>
2016-11-23 14:19:44 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,6 @@
 echo "Starting Auditd"
 
 exec /sbin/auditd -n
-#exec /sbin/auditctl -R /etc/audit/rules.d/custom.rules
-exec /sbin/auditctl -R /data/audit/custom.rules
-exec /sbin/auditctl -l
+exec /sbin/auditctl -R /etc/audit/rules.d/custom.rules
+#exec /sbin/auditctl -R /data/audit/custom.rules
+echo "exec /sbin/auditctl -l"
